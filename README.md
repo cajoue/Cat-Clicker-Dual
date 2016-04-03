6b679255278c24a8e3feed31b8def2de66d8266b
cajoue <cajoue@users.noreply.github.com>
2016-04-03 18:34:39 +0200

    feat: Create JSON for cats

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 Frontend Nanodegree project
 
 Duplicated original repo cat-clicker-1 using this process: https://help.github.com/articles/duplicating-a-repository/
+
+**New Requirements:** 
+* We need two cats
+* Cat name needs to be above the cat
--- js/main.js
+++ js/main.js
@@ -1,6 +1,27 @@
 'use strict';
 
+// cat JSON
+
+var cats = {
+    cat: [
+        {
+            id: 'cat01',
+            name: 'Katie',
+            images: 'http://placekitten.com/400/300'
+        },
+        {
+            id: 'cat02',
+            name: 'Charlotte',
+            images: 'http://placekitten.com/g/400/300'
+        }
+    ]
+};
+
+
+
 // use $(document).ready() for jQuery code in external js file
+// $(function(){}) is shorthand for $(document).ready(function(){})
+
 $(document).ready(function() {
   var numKitten = 0;
   $( '#kitten' ).click(function() {
