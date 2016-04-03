881340ed51e27c2f7ad43baa45c3b2d1263ccfcc
cajoue <cajoue@users.noreply.github.com>
2016-04-03 20:44:51 +0200

    feat: two cats with names

---
+++ js/helper.js
@@ -0,0 +1,12 @@
+// helper functions to display html
+
+var catUnit = '<div class="cat-unit"></div>';
+var catName = '<figure><figcaption>%data%</figcaption>';
+
+var catImage = '<picture><img src="%data%" alt="picture of kitten"></picture>';
+
+// var catSourceURL = '<figcaption>Kitten thanks to <a href="%data%">';
+// var catSource = '%data%</a></figcaption></figure>';
+var catSource = '<figcaption>Kitten thanks to <a href="#">%data%</a></figcaption></figure>';
+
+//var catID = '<a class="cat-id" href="#">%data%</a>'; // id
--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+
+
 // cat JSON
 
 var cats = {
@@ -7,15 +9,42 @@ var cats = {
         {
             id: 'cat01',
             name: 'Katie',
-            images: 'http://placekitten.com/400/300'
+            image: 'http://placekitten.com/400/300',
+            sourceURL: 'http://placekitten.com',
+            source: 'placekitten.com'
         },
         {
             id: 'cat02',
             name: 'Charlotte',
-            images: 'http://placekitten.com/g/400/300'
+            image: 'http://placekitten.com/g/400/300',
+            sourceURL: 'http://placekitten.com',
+            source: 'placekitten.com'
         }
     ]
 };
+
+cats.display = function(){
+  for (var kitty = 0, kittyCount = cats.cat.length; kitty < kittyCount; kitty++){
+    $('#cats').append(catUnit);
+
+    var formatCatName = catName.replace('%data%', cats.cat[kitty].name);
+    $('.cat-unit:last').append(formatCatName);
+
+    var formatCatImage = catImage.replace('%data%', cats.cat[kitty].image);
+      $('.cat-unit:last').append(formatCatImage);
+
+    //var formatCatSourceURL  = catSource.replace('%urldata%', cats.cat[kitty].sourceURL);
+      //$('.cat-unit:last').append(formatCatSourceURL);
+
+    var formatCatSource  = catSource.replace('%data%', cats.cat[kitty].source);
+      $('.cat-unit:last').append(formatCatSource);
+
+    //var formatCatID = catID.replace('%data%', cats.cat[kitty].id);
+     // $('.cat-unit:last').append(formatCatID);
+  }
+};
+
+
 
 
 
@@ -28,3 +57,5 @@ $(document).ready(function() {
     $( '#kitCount' ).text('I has been clicked ' + ++numKitten + ' times');
   });
 });
+
+cats.display();
